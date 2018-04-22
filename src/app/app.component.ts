2d245cb4ca73e280594d3cd34f297e3d64cb81c5
Abdessalem <abdessalem_benzarti@hotmail.fr>
2018-04-22 21:37:10 +0200

    Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  ngOnInit() {
+    $(document).foundation();
+    $('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
+      e.preventDefault();
+      $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
+    });
+  }
 }
