04c3eaa107dd64cdfedf317a7f10493b6af3fb40
Abdessalem <abdessalem_benzarti@hotmail.fr>
2018-05-17 14:29:53 +0200

    dashboard editing

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  @Input() comment: Comment;
+
+ comments: Array<Comment>;
+ title: String = 'General view of all comments';
+  uc = 'Number of unlabelled comments';
+  lc = 'Number of labelled comments';
+
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  getWidthProgressBar(comments: Comment) {
+    const len = comments.length;
+    return len + '%';
+  }
 }
